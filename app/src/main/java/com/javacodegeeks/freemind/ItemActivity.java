3554a27cec2e59670c39fba4041bccdfab5b3841
Anurag Chitnis <chitnis.anu@gmail.com>
2017-02-26 10:23:59 -0600

    Basic module working

--- app/src/main/java/com/javacodegeeks/freemind/ItemActivity.java
+++ app/src/main/java/com/javacodegeeks/freemind/ItemActivity.java
@@ -1,7 +1,16 @@
 package com.javacodegeeks.freemind;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import static com.javacodegeeks.freemind.QRCodeScanner.ACTION_SCAN;
 
 /**
  * Created by anura on 2/25/2017.
@@ -9,9 +18,59 @@ import android.os.Bundle;
 
 public class ItemActivity extends Activity {
 
+    private Button scanButton;
+    private HashMap<Integer,String> qrMap;
+    private TextView partNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+
+        partNumber = (TextView) findViewById(R.id.pickUpItem);
+        partNumber.setText("Part #1");
+
+        qrMap = new HashMap<>();
+        qrMap.put(1,"");
+        qrMap.put(2,"");
+        qrMap.put(3,"");
+        qrMap.put(4,"");
+        qrMap.put(5,"");
+
+        scanButton = (Button) findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanQRCode();
+            }
+        });
+
+
+    }
+
+    private void scanQRCode() {
+        Intent intent = new Intent(ACTION_SCAN);
+        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+        startActivityForResult(intent, 0);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                    if (!contents.equals("#01,square.obj,0,0,0,0,0,0")) { //#02,square.obj,0,0,0,0,0,0"
+                        scanQRCode();
+                    }
+                else {
+                        Toast toast = Toast.makeText(this, "Item Found, Pick it up", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+
+
+//                Intent itemIntent = new Intent(this, ItemActivity.class);
+//                startActivity(itemIntent);
+            }
+        }
     }
 }
--- app/src/main/java/com/javacodegeeks/freemind/QRCodeScanner.java
+++ app/src/main/java/com/javacodegeeks/freemind/QRCodeScanner.java
@@ -8,13 +8,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public class QRCodeScanner extends Activity {
 	/** Called when the activity is first created. */
@@ -22,50 +19,21 @@ public class QRCodeScanner extends Activity {
 	static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
 	private DatabaseReference mDatabase;
 	private DatabaseReference boothRef;
+	private TextView boothTextView;
+	private boolean isRepeatedMode;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Write a message to the database
-		mDatabase = FirebaseDatabase.getInstance().getReference();
-		FirebaseDatabase database = FirebaseDatabase.getInstance();
-		DatabaseReference myRef = database.getReference("Booth");
-		ArrayList<Part> partArrayList = new ArrayList<>();
-		ArrayList<Module> moduleArrayList = new ArrayList<>();
-		boothRef = mDatabase.child("booth");
-
-		Random rand = new Random();
-		for(int i=0;i<10;i++) {
-			int num = rand.nextInt(100000) + 9999;
-
-			Part part = new Part("part"+num);
-			partArrayList.add(part);
-
-		}
-
-		for(int i=0;i<10;i++) {
-			int num = rand.nextInt(100000) + 9999;
-
-			Module module1 = new Module("module"+num);
-			moduleArrayList.add(module1);
-
-		}
-		Module module1 = new Module();
-		module1.setPartsList(partArrayList);
-
-		Booth booth = new Booth();
-		booth.setModuleList(moduleArrayList);
-
-
-
-		boothRef.setValue(booth);
+		//boothTextView = (TextView) findViewById(R.id.);
 
 	}
 
 	public void scanBar(View v) {
 		try {
+			isRepeatedMode = true;
 			Intent intent = new Intent(ACTION_SCAN);
 			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
 			startActivityForResult(intent, 0);
@@ -76,6 +44,7 @@ public class QRCodeScanner extends Activity {
 
 	public void scanQR(View v) {
 		try {
+			isRepeatedMode = false;
 			Intent intent = new Intent(ACTION_SCAN);
 			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
 			startActivityForResult(intent, 0);
@@ -111,12 +80,16 @@ public class QRCodeScanner extends Activity {
 			if (resultCode == RESULT_OK) {
 				String contents = intent.getStringExtra("SCAN_RESULT");
 				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-				if(!contents.equals("bobcob")){
-					scanQR(null);
+				if(isRepeatedMode) {
+					if (!contents.equals("bobcob")) {
+						scanQR(null);
+					}
 				}
 
-				Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+				Toast toast = Toast.makeText(this, "Content:" + contents, Toast.LENGTH_LONG);
 				toast.show();
+				Intent itemIntent = new Intent(this, ItemActivity.class);
+				startActivity(itemIntent);
 			}
 		}
 	}
