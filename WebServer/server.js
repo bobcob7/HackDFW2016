b3025bd18796629f527c221f45602cce5c5e4b61
bobcob7 <bobcob7@hotmail.com>
2017-02-26 13:42:22 +0000

    Added an awk command

--- WebServer/server.js
+++ WebServer/server.js
@@ -27,7 +27,7 @@ function confirm_request (req, res) {
 function findBoothQR (req, res) {
   // var qrcode = req.body.qrcode;
   console.log('SELECT * FROM booths WHERE Id='+req.body.qrcode);
-  connection.query('SELECT parts.Id FROM booths INNER JOIN modules INNER JOIN parts WHERE booths.Id=? AND FOUND!=1 AND ASSIGNED!=1 LIMIT 1;', req.body.qrcode,
+  connection.query('SELECT PartId FROM UBooth INNER JOIN UMod ON UBooth.ModId=UMod.ModId INNER JOIN parts ON UMod.PartId=parts.Id WHERE BoothId=? AND found=0 AND assigned=0 LIMIT 1;', req.body.qrcode,
   function(err, result) {
       if (err) {
         console.log(result);
@@ -42,6 +42,20 @@ function findBoothQR (req, res) {
   //console.log(qrcode);
 }
 
+function ackPartId (req, res) {
+  var partId = req.body.partId;
+  console.log('SELECT * FROM booths WHERE Id='+req.body.qrcode);
+  connection.query('UPDATE parts SET assigned=1 WHERE available=0 AND assigned=0 AND parts.Id=?;', partId,
+  function(err, result) {
+      if (err) {
+        console.log(result);
+        throw err;
+      }
+  }
+});
+  //console.log(qrcode);
+}
+
 function send_items (req, res) {
   res.send()
 };
@@ -49,6 +63,8 @@ function send_items (req, res) {
 app.get('/', confirm_request);
 
 app.post('/registerBoothQR/', findBoothQR);
+
+app.post('/ack', ackPartId);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
