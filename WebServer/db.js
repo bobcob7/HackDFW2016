6aa1f259d539db632091363a92db7e2cbb213991
Chris Smith <christophersmith14@my.unt.edu>
2017-02-25 23:19:13 -0600

    fixed function

--- WebServer/db.js
+++ WebServer/db.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql'),
     async = require('async');
 
-exports.connection = function ()
+exports.connection = function () {
   var mysql = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : 'suedmeier',
     database : 'parts_list'
-});
+  });
+}
