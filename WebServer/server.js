c577cb1d39b5cd337b4707442533481ddd43ae08
Chris Smith <christophersmith14@my.unt.edu>
2017-02-26 01:42:30 -0600

    qrcode search in db

--- WebServer/server.js
+++ WebServer/server.js
@@ -25,11 +25,11 @@ function confirm_request (req, res) {
 };
 
 function findBoothQR (req, res) {
-  var qrcode = req.body.qrcode;
-  connection.query('SELECT * FROM booths WHERE Id = ?', qrcode,
-    function(err, res) {
+  // var qrcode = req.body.qrcode;
+  connection.query('SELECT * FROM booths WHERE Id = ?', req.body,
+    function(err, result) {
       // if (err) throw err;
-      res.end('Your QRCode is ' + qrcode);
+      res.send('Your QRCode is ' + result.insertId);
   });
   console.log(qrcode);
 }
