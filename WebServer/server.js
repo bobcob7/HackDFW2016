d7453470ecd87a8939522d7845032ee781fdf3f6
Chris Smith <christophersmith14@my.unt.edu>
2017-02-26 01:46:12 -0600

    error checking db search

--- WebServer/server.js
+++ WebServer/server.js
@@ -28,7 +28,10 @@ function findBoothQR (req, res) {
   // var qrcode = req.body.qrcode;
   connection.query('SELECT * FROM booths WHERE Id = ?', req.body,
     function(err, result) {
-      // if (err) throw err;
+      if (err) {
+        console.log(result.insertId);
+        throw err;
+      }
       res.send('Your QRCode is ' + result.insertId);
   });
   console.log(qrcode);
